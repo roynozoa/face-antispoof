82d614b2679acb5fdfab4a41fdb520f88aee40f9
Muhammad Adisatriyo Pratama <i_muhammad_pratama@linkaja.io>
2021-04-27 22:57:59 +0700

    add notebooks

---
+++ README.md
@@ -0,0 +1,17 @@
+# Face Anti-Spoofing with TensorFlow
+
+Muhammad Adisatriyo Pratama - May 2021
+____________________
+## Face Antispoofing using Celeb-A Spoof dataset with TensorFlow
+
+CelebA-Spoof Dataset Reference:
+- https://github.com/Davidzhangyuanhan/CelebA-Spoof
+
+```script
+@inproceedings{CelebA-Spoof,
+  title={CelebA-Spoof: Large-Scale Face Anti-Spoofing Dataset with Rich Annotations},
+  author={Zhang, Yuanhan and Yin, Zhenfei and Li, Yidong and Yin, Guojun and Yan, Junjie and Shao, Jing and Liu, Ziwei},
+  booktitle={European Conference on Computer Vision (ECCV)},
+  year={2020}
+}
+```
---
+++ make_dataset.py
@@ -0,0 +1,42 @@
+import shutil
+import os
+import pandas as pd 
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+TRAIN_LABEL_TXT = 'metas/intra_test/train_label.txt'
+TEST_LABEL_TXT = 'metas/intra_test/test_label.txt'
+
+
+def create_train():
+    logging.info('Copying train dataset ...')
+    train_label_df = pd.read_csv(TRAIN_LABEL_TXT, header=None, delimiter=' ')
+    train_dir_list = train_label_df[0].tolist()
+    train_label_list = train_label_df[1].tolist()
+    i = 0
+    for source in train_label_df[0].tolist():
+        if train_label_df[1].tolist()[i] == 0:
+            shutil.copy(source, 'dataset/train/real')
+        else:
+            shutil.copy(source, 'dataset/train/spoof')
+        i += 1
+
+def create_test():
+    logging.info('Copying test dataset ...')
+    test_label_df = pd.read_csv(TEST_LABEL_TXT, header=None, delimiter=' ')
+    test_dir_list = test_label_df[0].tolist()
+    test_label_list = test_label_df[1].tolist()
+    i = 0
+    for source in test_label_df[0].tolist():
+        if test_label_df[1].tolist()[i] == 0:
+            shutil.copy(source, 'dataset/test/real')
+        else:
+            shutil.copy(source, 'dataset/test/spoof')
+        i += 1
+
+if __name__ == '__main__':
+   logging.info('Creating dataset ...')
+   create_train()
+   create_test()
