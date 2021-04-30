8b2f427da58ff67bfa8300ca266012b908d92005
Muhammad Adisatriyo Pratama <muhammad.adisatriyo@gmail.com>
2021-04-30 22:23:35 +0700

    add different model

---
+++ demo/demo.py
@@ -0,0 +1,11 @@
+# Face Antispoofing Demo and Testing
+# Muhammad Adisatriyo Pratama - May 2021
+
+
+# Import library
+import os
+import time
+import numpy as np
+import pandas as pd 
+import tensorflow as tf
+import cv2 as cv
