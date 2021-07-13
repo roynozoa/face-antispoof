3187871cf9a4e6868fbc3c406f24883522c8c9a7
Muhammad Adisatriyo Pratama <muhammad.adisatriyo@gmail.com>
2021-07-13 16:13:11 +0700

    update demo and image

--- demo/api/main.py
+++ demo/api/main.py
@@ -57,4 +57,4 @@ def process_image():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
--- demo/demo.py
+++ demo/demo.py
@@ -89,9 +89,9 @@ vs = VideoStream(src=0).start()
 # loop over the frames from the video stream
 while True:
 	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
+	# to have a maximum width of 500 pixels
 	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	frame = imutils.resize(frame, width=500)
 
 	# detect faces in the frame and determine if they are wearing a
 	# face mask or not
